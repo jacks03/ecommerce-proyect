0156092c9ee0fe0e215a5c12adaaf5ed63573052
jacks03 <kjarisalfaro14@gmail.com>
2023-02-13 00:17:11 -0500

    avance 4

--- src/components/mainpage/Home.jsx
+++ src/components/mainpage/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Categories from './Categories'
-import SlideCard from './SlideCard'
+import Slider from './Slider'
+import "./Home.css"
 
 const Home = () => {
     
@@ -9,7 +10,7 @@ const Home = () => {
     <section className="home">
         <div className="container d_flex">
             <Categories/>
-            <SlideCard/>
+            <Slider/>
         </div>
     </section>
     </>
--- src/components/mainpage/Slider.jsx
+++ src/components/mainpage/Slider.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import SlideCard from "./SlideCard"
 
 const Slider = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <section className="homeSlide contentWidth">
+        <div className="container">
+          <SlideCard/>
+        </div>
+      </section>
+    </>
   )
 }
 
