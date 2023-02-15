1f8afe6e854c046b83ee59c3aec4d8c7240738d6
jacks03 <kjarisalfaro14@gmail.com>
2023-02-14 21:48:43 -0500

    finalfallido

--- src/components/mainpage/Home.jsx
+++ src/components/mainpage/Home.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import Categories from './Categories'
-import Slider from './Slider'
+import React from "react"
+import Categories from "./Categories"
 import "./Home.css"
+import SliderHome from "./Slider"
 
 const Home = () => {
-    
   return (
     <>
-    <section className="home">
-        <div className="container d_flex">
-            <Categories/>
-            <Slider/>
+      <section className='home'>
+        <div className='container d_flex'>
+          <Categories />
+          <SliderHome />
         </div>
-    </section>
+      </section>
     </>
   )
 }
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Home from '../components/mainpage/Home'
+import Home from '../components/MainPage/Home'
 import FlashDeals from '../components/flashDeals/FlashDeals'
 const pages = ({ productsItems }) => {
   return (
